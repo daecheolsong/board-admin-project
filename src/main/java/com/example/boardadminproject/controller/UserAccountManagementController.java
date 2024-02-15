ac14d8a2ac4c9386d3db4ae8cd36196b2f698fb8
daecheolsong <eocjf8905@naver.com>
2024-02-15 15:12:17 +0900

    #16 - 회원 관리 페이지에 템플릿 레이아웃, DataTable 적용
    회원 관리 페이지를 #10 에서 만든 템플릿 레이아웃 기반으로 작성하고, DataTable 플러그인을 적용하였다.
    
    그리고 템플릿 네이밍 케이스를 camelCase 에서 kebabCase 로 변경하였다.

--- src/main/java/com/example/boardadminproject/controller/UserAccountManagementController.java
+++ src/main/java/com/example/boardadminproject/controller/UserAccountManagementController.java
@@ -19,6 +19,6 @@ public class UserAccountManagementController {
     @GetMapping
     public String userAccounts(@PageableDefault(size = 10, sort = "createdAt", direction = Sort.Direction.DESC) Pageable pageable,
                                Model model) {
-        return "management/userAccounts";
+        return "management/user-accounts";
     }
 }
